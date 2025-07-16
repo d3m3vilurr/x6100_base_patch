25be3bf632ca4db131d5f7b260a54491324e613c
Sunguk Lee <d3m3vilurr@gmail.com>
2025-07-17 01:04:46 +0900

    Fix return type of functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,11 +56,11 @@ void SystemClock_Config(void);
 
 /* USER CODE END 0 */
 
-extern int am_fm_rx_process(void *, void *, void*);
-extern int configure();
-extern int tx_amp(void *, void*);
-extern int tx_coeff_calc(float);
-extern int anf_update(void *, void*, void*, int);
+extern float am_fm_rx_process(void *, void *, void*);
+extern void configure();
+extern void tx_amp(float*, float*);
+extern void tx_coeff_calc(float);
+extern void anf_update(void *, void*, void*, int);
 /**
   * @brief  The application entry point.
   * @retval int
